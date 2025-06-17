0c8e365f6c702c3b17158b9ac39deddc727027f0
Spencer Cain <cainspencerm@protonmail.com>
2025-06-17 15:32:42 -0400

    feat: set the default font to STIX Two Text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,35 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from "next"
+import "./globals.css"
+
+import { cn } from "@/utils/cn"
+import { STIX_Two_Text } from "next/font/google"
+
+const stix = STIX_Two_Text({
+  subsets: ["latin"],
+  variable: "--font-stix-two-text",
+})
 
 export const metadata: Metadata = {
   title: "Spencer Cain — Software Engineer",
-  description: "The website of Spencer Cain, a software engineer and technical writer. Explore engineering projects, technical insights, and original writing across development, systems, and creative technology.",
-};
+  description:
+    "The website of Spencer Cain, a software engineer and technical writer. Explore engineering projects, technical insights, and original writing across development, systems, and creative technology.",
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body
-        className="w-screen min-h-screen flex justify-center antialiased"
+        className={cn(
+          "w-screen min-h-screen flex justify-center antialiased",
+          stix.className
+        )}
       >
         {children}
       </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="container flex flex-1 items-center justify-center">
-      Home
+      <h1>Home</h1>
     </main>
-  );
+  )
 }
