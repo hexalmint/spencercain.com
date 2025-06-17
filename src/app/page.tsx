f5ec743cf536a1ce6892b89819c241a2552c0688
Spencer Cain <cainspencerm@protonmail.com>
2025-06-17 15:43:29 -0400

    style: add the prose class to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="container flex flex-1 items-center justify-center">
-      Home
+    <main className="container prose prose-invert flex flex-1 items-center justify-center">
+      <h1>Home</h1>
     </main>
-  );
+  )
 }
