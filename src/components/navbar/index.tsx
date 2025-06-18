5dfcea5f6b2b4a3de3a0579aac030876b80004b0
Spencer Cain <cainspencerm@protonmail.com>
2025-06-18 16:05:23 -0400

    feat: create a responsive navbar for desktop and mobile

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,52 @@
+import { NavbarMenu } from "@/components/navbar/navbar-menu"
+import Link from "next/link"
+import React from "react"
+
+export function Navbar({
+  brand,
+  items,
+}: {
+  brand: React.ReactNode
+  items?: {
+    element: React.ReactNode
+    href: string
+  }[]
+}) {
+  return (
+    <nav className="w-full h-14 grid grid-cols-2 sticky top-0 py-2 px-4 bg-background">
+      <div className="flex items-center px-2">{brand}</div>
+
+      {/* Desktop Navbar Items */}
+      <ul className="hidden sm:flex items-center justify-end gap-2 ">
+        {items &&
+          items.map((item, index) => (
+            <li key={`navbar-${index}`}>
+              <NavbarItem href={item.href}>{item.element}</NavbarItem>
+            </li>
+          ))}
+      </ul>
+
+      {/* Mobile Navbar Items */}
+      <div className="flex items-center justify-end sm:hidden">
+        <NavbarMenu items={items} />
+      </div>
+    </nav>
+  )
+}
+
+function NavbarItem({
+  children,
+  href,
+}: {
+  children: React.ReactNode
+  href: string
+}) {
+  return (
+    <Link
+      href={href}
+      className="w-fit min-w-20 flex justify-center items-center rounded-xl py-2 px-4 transition-colors duration-150 hover:bg-foreground/10 active:bg-foreground/10"
+    >
+      {children}
+    </Link>
+  )
+}
