330d9643b6fabc6b5d230c8309d563bc1fc3f91c
Spencer Cain <cainspencerm@protonmail.com>
2025-06-18 16:11:27 -0400

    fix: render body children column-wise

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className="w-full min-h-screen flex justify-center antialiased"
+        className="w-full min-h-screen flex flex-col items-center antialiased"
       >
         <Navbar
           brand={
