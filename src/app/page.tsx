5061a247b0476d93e5ded358c3a560b09b1d7a3a
Spencer Cain <cainspencerm@protonmail.com>
2025-06-18 16:13:49 -0400

    feat: add lorem ipsum to the home page for visual testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,117 @@
 export default function Home() {
   return (
-    <main className="container prose prose-invert flex flex-1 items-center justify-center">
+    <main className="prose lg:prose-lg prose-invert max-w-none w-4xl">
       <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+        ullamcorper interdum erat, sed accumsan massa aliquam quis. Phasellus
+        tristique eros tortor. Quisque eu sollicitudin mauris. Proin in tortor
+        nec risus gravida malesuada. Quisque ut scelerisque justo. Quisque
+        tristique velit risus, eu bibendum est finibus at. Integer eu mauris sed
+        mi tristique cursus. Sed urna eros, fermentum vitae magna ac, gravida
+        dictum quam. Sed vulputate massa a leo hendrerit, a sodales sapien
+        mollis. Duis sagittis sodales dui quis viverra. Cras lobortis elit eu
+        scelerisque elementum. Maecenas accumsan ex ante, sit amet mattis ipsum
+        auctor at.
+      </p>
+      <p>
+        Nam justo ante, tempus sed ex ac, luctus suscipit felis. Vivamus leo
+        tellus, iaculis sed porta non, pulvinar id nibh. Morbi ultricies velit
+        vel semper cursus. Proin varius neque vel lectus imperdiet venenatis.
+        Cras sed molestie diam. Suspendisse posuere fringilla enim, vitae
+        consectetur erat egestas in. Etiam lacinia mi varius ullamcorper varius.
+        Quisque vehicula commodo diam, vel vestibulum ante finibus eget.
+        Pellentesque molestie erat aliquet venenatis vehicula. Integer interdum
+        augue et consectetur semper. Nullam ipsum orci, dignissim vel purus eu,
+        convallis consequat lacus. Aliquam egestas pellentesque lacus nec
+        interdum. Aliquam erat volutpat.
+      </p>
+      <p>
+        Ut suscipit auctor finibus. Aliquam erat volutpat. Donec volutpat
+        scelerisque ante ac dignissim. Sed sit amet turpis molestie, viverra
+        nisi eget, rutrum risus. Mauris vitae fringilla urna, vitae blandit
+        libero. Nunc enim neque, rhoncus vel nunc sed, pretium posuere tortor.
+        Phasellus sit amet elementum mauris. Fusce sed ligula nec risus dictum
+        placerat vitae vel metus. Suspendisse quis metus sem. Nunc elit nisl,
+        elementum nec nisi ut, gravida posuere nisi.
+      </p>
+      <p>
+        Morbi scelerisque mi pulvinar, euismod sapien vel, ullamcorper turpis.
+        Mauris faucibus nisi eu ante commodo finibus. Fusce sed molestie sem.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at
+        condimentum massa. Vestibulum mollis velit nibh, sed consequat mauris
+        bibendum ut. Pellentesque malesuada felis at egestas viverra. Nunc sed
+        ex in tortor congue efficitur.
+      </p>
+      <p>
+        Donec sit amet finibus magna. Nullam pulvinar odio et nulla ullamcorper,
+        at imperdiet diam imperdiet. Nam lacus sapien, elementum et leo eget,
+        efficitur sodales mi. Aliquam pulvinar consequat est, a vestibulum sem.
+        Nullam malesuada mattis mauris id dignissim. Nunc tempor ante at lacus
+        sagittis venenatis. Duis leo velit, consectetur eu pulvinar at, eleifend
+        non lorem. Proin quis euismod dui. Fusce sit amet tortor id sem congue
+        tempus non sed lorem. Vivamus molestie mi et eros tincidunt vestibulum.
+        Morbi ut ornare tortor, id facilisis elit. Cras vel nunc dapibus, congue
+        lorem eget, iaculis felis.
+      </p>
+      <p>
+        Curabitur convallis pharetra sem, vel consectetur lorem ultricies non.
+        Mauris lacinia ac ipsum quis lacinia. Duis sollicitudin tincidunt odio,
+        in mattis eros ornare a. Praesent vitae sem et mi posuere vestibulum.
+        Aenean eu urna nec odio iaculis placerat. Morbi posuere pellentesque
+        tortor vitae tincidunt. Praesent sit amet augue eros. Suspendisse et
+        mattis ante, sed luctus nisi. Nam leo diam, scelerisque non laoreet ut,
+        hendrerit quis ex. In sapien risus, vestibulum bibendum pulvinar non,
+        varius ac neque. Phasellus at interdum quam. Suspendisse faucibus
+        feugiat iaculis. Ut porta ex risus, ac tempor urna consequat sit amet.
+        Etiam nibh lacus, dapibus vel urna id, commodo egestas justo. Aenean
+        sapien orci, egestas nec augue eu, iaculis tempus risus. Fusce blandit
+        ultricies elit ac pellentesque.
+      </p>
+      <p>
+        Pellentesque at libero vel nulla posuere dignissim eu quis ipsum.
+        Quisque in placerat tortor. In egestas sed tortor quis mattis. Morbi
+        aliquam maximus ipsum, et varius odio blandit at. Proin quis pharetra
+        orci, non mollis nibh. Donec accumsan lacus libero, ac pulvinar augue
+        pulvinar a. Proin sit amet est tincidunt, consequat orci quis, maximus
+        augue. Donec dapibus arcu a libero imperdiet, eu ultricies turpis
+        vestibulum.
+      </p>
+      <p>
+        Donec pharetra justo eu libero porta rutrum. Vivamus congue quis erat a
+        accumsan. Mauris fermentum quis urna in vestibulum. Aenean luctus nec
+        nisi et egestas. Pellentesque pretium tellus eget hendrerit cursus. Duis
+        egestas sollicitudin libero, quis tempus sapien feugiat vel. Nulla
+        facilisi. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        Fusce porttitor vel elit a egestas. Vestibulum convallis luctus risus,
+        sed varius leo blandit at. In quis posuere purus. Nulla fringilla augue
+        vel sagittis aliquam. Etiam vel fermentum felis. Interdum et malesuada
+        fames ac ante ipsum primis in faucibus. Sed sodales purus non imperdiet
+        facilisis.
+      </p>
+      <p>
+        Maecenas eu orci augue. Vestibulum laoreet eros a neque pulvinar
+        aliquam. Suspendisse leo nulla, sodales nec ligula vel, eleifend posuere
+        sem. Donec ornare lorem urna, vel egestas est lobortis at. Nulla
+        lacinia, orci et sagittis sollicitudin, erat ligula mattis ex, in
+        lobortis nibh risus in tellus. Vivamus ipsum est, dictum quis ligula at,
+        eleifend sollicitudin risus. Etiam augue lectus, pulvinar vel arcu et,
+        elementum dignissim massa. Nunc sed justo sem. Pellentesque egestas
+        malesuada mauris quis consequat. Suspendisse laoreet porta diam, eu
+        sollicitudin libero accumsan sed. Nulla ut urna sit amet nibh congue
+        luctus. Proin lacinia dolor et justo bibendum maximus. In hac habitasse
+        platea dictumst. Etiam tincidunt fringilla dapibus.
+      </p>
+      <p>
+        Phasellus porta lectus cursus scelerisque vestibulum. Nullam efficitur
+        luctus libero sed elementum. Aliquam eu elit eu tellus rutrum molestie
+        vel a eros. Phasellus mollis nisl lectus, at suscipit ipsum semper eget.
+        Donec vulputate libero quis tellus dignissim, a molestie est lacinia.
+        Nunc laoreet facilisis lacinia. Quisque non dolor magna. Proin malesuada
+        lorem magna, sit amet ultrices sem lacinia at. Fusce tristique nisi
+        interdum tellus pretium gravida. Morbi sed mi varius, ullamcorper odio
+        a, aliquam ligula. Integer eu risus sem.
+      </p>
     </main>
   )
 }
